a1ef56561bea2cd4a6abefabcc8d9d813692bcc8
bjoerm <bjoern-menke@web.de>
2020-05-17 16:59:52 +0200

    Renaming main file.

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@
 # import typing
 import toml
 
-from Environment import Environment
+from environment import Environment
 
 
 
@@ -15,10 +15,10 @@ def main():
     # Get options
     cfg = toml.load("options.toml", _dict=dict)
 
-    # TODO cfg.get('selected_id3_fields')
+    # TODO cfg.get("selected_id3_fields")    
 
     # Set up environment, convert them to lowercase file extension and get list of mp3 files.
-    env = Environment(input_path="untouched_input", wip_path="input")
+    env = Environment(input_path=cfg.get("input_path"), wip_path=cfg.get("wip_path"))
 
     print(env.files_and_folders)
 
