eee3d5b198d7b0f05ec9d4075b1ba1c598f62ccd
bjoerm <bjoern-menke@web.de>
2020-12-22 20:57:41 +0100

    Converted class into utility class.

--- __main__.py
+++ __main__.py
@@ -1,12 +1,10 @@
 # TODO Add log files with tags prior and after change.
 # TODO Add progress in percent calculated based on the share of finished folders.
 
-# Restore # TODO The following part needs to be restored! Checking the file path for a "cd xy" string. in beautify_tags.py
-
 import toml
 
 from environment import Environment
-from tag_management import ProcessMp3
+from tag_management import TagManager
 
 
 
@@ -19,9 +17,10 @@ def main():
     # Set up environment, convert them to lowercase file extension and get list of mp3 files and their folder.
     env = Environment(input_path=cfg.get("input_path"), wip_path=cfg.get("wip_path"))
 
-    tags = ProcessMp3(selected_id3_fields=cfg.get("selected_id3_fields"), files_and_folders=env.files_and_folders)
+    # Improve the MP3 ID3 tags in the specified folder(s).
+    # tags = TagManager(selected_id3_fields=cfg.get("selected_id3_fields"), files_and_folders=env.files_and_folders)
     
-    tags.improve_tags()
+    TagManager.improve_tags(selected_id3_fields=cfg.get("selected_id3_fields"), files_and_folders=env.files_and_folders)
 
 
 if __name__ == "__main__":
