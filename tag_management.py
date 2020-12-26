f89029a729ea7e9ce7aff60bd6940f29275a5285
bjoerm <bjoern-menke@web.de>
2020-12-26 22:35:59 +0100

    Switched to tag selection in the options from a dict to a list.
    Merged select tags and copy selected tags into a single method.

--- tag_management.py
+++ tag_management.py
@@ -10,7 +10,7 @@ class TagManager:
 
 
     @classmethod
-    def improve_tags(cls, selected_id3_fields: list, files_and_folders: list):
+    def improve_tags(cls, selected_id3_fields: list, files_and_folders: pd.DataFrame):
         unique_mp3_folders = cls._get_unique_mp3_folders(files_and_folders=files_and_folders)
         
         # Work from folder to folder.
@@ -19,19 +19,19 @@ class TagManager:
             
             df_iteration = None # Cleaning the iteration df at the start of each loop.
             
-            df_iteration = files_and_folders[(files_and_folders.folder == i)] # Selecting the files in the i'th unique folder. # TODO Should this be done via .loc?
+            df_iteration = cls._select_current_folder(files_and_folders=files_and_folders, current_unique_folder=i)
+
             
             # Read all tags in folder
-            df_iteration = cls.read_id3_tags_in_folder(df_iteration = df_iteration)
+            df_iteration = cls._read_id3_tags_in_folder(df_iteration=df_iteration)
             
             
-            # Select and copy specified tag fields.
-            df_iteration = cls.select_tags(df_iteration=df_iteration, selected_id3_fields=selected_id3_fields)
-            df_iteration = cls.copy_selected_tags(df_iteration=df_iteration)
+            # Keep only selected tags
+            df_iteration = cls._keep_only_selected_tags(df_iteration=df_iteration, selected_id3_fields=selected_id3_fields)
             
             
             # Remove encoding information and only keep the text.
-            df_iteration["unchanged_tag"] = cls.remove_string_encoding_information(tags=df_iteration["unchanged_tag"].copy())
+            df_iteration["unchanged_tag"] = cls._remove_string_encoding_information(tags=df_iteration["unchanged_tag"])
             
             
             # Pass tag on to the beautifier utility class.
@@ -41,12 +41,13 @@ class TagManager:
             # Delete all existing tags from the ID3 object.
             [df_iteration["id3"][i].delete() for i in df_iteration.index]
 
-
+            # Create new ID3 object from beautified tag.
             df_iteration["id3"] = [cls._save_tags_to_single_id3_object(id3=df_iteration["id3"][i], beautified_tag=df_iteration["beautified_tag"][i]) for i in df_iteration.index]
 
             
             # Write beautified tag to file.
             [df_iteration["id3"][i].save(v1 = 0, v2_version = 4) for i in df_iteration.index] # Saving only as id3v2.4 tags and not as id3v1 at all.
+    
     
     
     @staticmethod
@@ -61,17 +62,28 @@ class TagManager:
         
         return(unique_mp3_folders)
 
+
+    @staticmethod
+    def _select_current_folder(files_and_folders: pd.DataFrame, current_unique_folder) -> pd.DataFrame:
+        """
+        Selecting the files in the i'th unique folder.
+        """
+        
+        df_iteration = files_and_folders[(files_and_folders.folder == current_unique_folder)].copy() # The .copy() is required to have a real copy and not just a view of the list.
+        
+        df_iteration = df_iteration.reset_index(drop=True)
+        
+        return(df_iteration)
+
+
     
     @classmethod
-    def read_id3_tags_in_folder(cls, df_iteration: pd.DataFrame) -> pd.DataFrame: # TODO Could get rid of the cls, once _attach_column_to_df_iteration is removed/replaced.
+    def _read_id3_tags_in_folder(cls, df_iteration: pd.DataFrame) -> pd.DataFrame: # TODO Could get rid of the cls, once _attach_column_to_df_iteration is removed/replaced.
         """
         Read the whole id3 tags for all provided files.
         """
 
-        id3 = [cls._read_id3_tag_from_single_file(i) for i in df_iteration["file"]] # Read all tags.
-        
-        # Attaching a column with the tag that was read.
-        df_iteration = cls._attach_column_to_df_iteration(data_for_series=id3, series_name="id3", df_iteration=df_iteration)
+        df_iteration["id3"] = [cls._read_id3_tag_from_single_file(i) for i in df_iteration["file"]] # Read all tags.
         
         return(df_iteration)
 
@@ -88,47 +100,32 @@ class TagManager:
             return None
 
         return(id3) # Returns an ID3 object.
-    
-    
-    
-    @classmethod
-    def select_tags(cls, df_iteration: pd.DataFrame, selected_id3_fields: list) -> pd.DataFrame: # TODO Could get rid of the cls, once _attach_column_to_df_iteration is removed/replaced.
-        """
-        Select the specified tag fields.
-        """
-        
-        unchanged_tag = [list(i.keys() & selected_id3_fields.keys()) for i in df_iteration["id3"]]
-        
-        # Attaching a column with the selected tag.
-        df_iteration = cls._attach_column_to_df_iteration(data_for_series=unchanged_tag, series_name="unchanged_tag", df_iteration=df_iteration)
-        
-        return(df_iteration)
-    
+
+
     
     @staticmethod
-    def copy_selected_tags(df_iteration: pd.DataFrame) -> pd.DataFrame:
+    def _keep_only_selected_tags(df_iteration: pd.DataFrame, selected_id3_fields: list) -> pd.DataFrame:
         """
-        Copying/filling the original tag information (but only) for the selected tag fields.
+        Creating a copy of the original tag information (but only) for the selected tag fields.
         """
+        
+        # TODO This should be refactored to have only the id3 column (pd.Series), where every entry is a dictionary per file, as input and return an pd.Series of the same shape.
         
         unchanged_tag = [
-            dict(
-                (
-                    k
-                    , df_iteration["id3"][i].get(k)) for k in df_iteration["unchanged_tag"][i] if k in df_iteration["id3"][i]
-                ) for i in df_iteration.index
-            ] # Nested list comprehension adaptation of https://stackoverflow.com/questions/6827834/how-to-filter-a-dict-to-contain-only-keys-in-a-given-list.
+            {
+                k:v for (k, v) in df_iteration["id3"][i].items() if k in selected_id3_fields
+            } # Selecting only the tags that are specified in selected_id3_fields
+            for i in df_iteration.index
+            ]
         
-        # Updating the column with the selected tag.
         df_iteration["unchanged_tag"] = unchanged_tag
-        
         
         return(df_iteration)
     
     
     
     @staticmethod
-    def remove_string_encoding_information(tags: pd.Series) -> pd.Series:
+    def _remove_string_encoding_information(tags: pd.Series) -> pd.Series:
         """
         Getting rid of the encoding part and only keeping the "text", so it will later be unifiedly be saved as UTF8 and not e.g. LATIN1. So changing 'TALB(encoding=<Encoding.LATIN1: 0>, text=['Stellaris : Ancient Relics'])' into a simple dictionary 'TALB: 'Stellaris : Ancient Relics'). 
         """
