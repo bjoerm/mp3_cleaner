23ddca9761e672ec708dc6affd69fd2b28845275
bjoerm <bjoern-menke@web.de>
2020-12-29 20:54:30 +0100

    Better tests and more descriptions.

--- test_pytest.py
+++ test_pytest.py
@@ -4,10 +4,15 @@ from beautify_date import beautify_date
 from tag_management import TagManager
 
 def test_string_beautification():
+    """
+    This tests the main/public method for beautifying strings. It contains all the (private) methods related to strings.
+    """
+    assert beautify_string.beautify_string("") == ""
     assert beautify_string.beautify_string("Test's test's test`s test´s test'S") == "Test's Test's Test's Test's test's", "Test hyphens"
     assert beautify_string.beautify_string("Ac/Dc AC/DC ac/dc guns 'n' roses") == "ACDC ACDC ACDC Guns 'n' Roses", "Test edge cases"
     assert beautify_string.beautify_string(" lost in      space  Lost in Space ") == "Lost In Space Lost In Space", "Test spaces"
     assert beautify_string.beautify_string("where to? what now???") == "Where To What Now", "Question mark"
+    assert beautify_string.beautify_string("?!?") == "!", "Question mark"
     assert beautify_string.beautify_string("I'am") == "I'am", "Test connected words"
     assert beautify_string.beautify_string("data/now data\yesterday") == "Data-Now Data-Yesterday", "Test slash"
     assert beautify_string.beautify_string("A feat. B C featuring D E feat F") == "A Feat. B C Feat. D E Feat F", "Test Featuring"
@@ -27,39 +32,42 @@ def test_string_beautification():
     assert beautify_string.beautify_string(" Pt. 2") == "Part 2", "Test Part 1"
     assert beautify_string.beautify_string("Pt. 2") == "Part 2", "Test Part 2"
     assert beautify_string.beautify_string("Bomb Pt. 2") == "Bomb Part 2", "Test Part 3"
+    assert beautify_string.beautify_string("test III") == "Test III"
     assert beautify_string.beautify_string(None) == None, "Test none"
 
 
-def test_string_beautification_leading_the(input=True): # Switch this to True to easily test it also for that case.
-    assert beautify_string.beautify_string("The Beatles ", remove_leading_the=input) == "Beatles", "Test Leading The"
-    assert beautify_string.beautify_string(" The Beatles ", remove_leading_the=input) == "Beatles", "Test Leading The"
-    assert beautify_string.beautify_string("TheBeatles ", remove_leading_the=input) == "TheBeatles", "Test Leading The"
-    assert beautify_string.beautify_string("The-Beatles ", remove_leading_the=input) == "The-Beatles", "Test Leading The"
-    assert beautify_string.beautify_string(None, remove_leading_the=input) == None, "Test none"
-    assert beautify_string.beautify_string("The", remove_leading_the=input) == "The", "Test for a band named 'The'"
+def test_string_beautification_leading_the(remove_leading_the=True): # Switch this to True to easily test it also for that case.
+    assert beautify_string.beautify_string("", remove_leading_the=remove_leading_the) == ""
+    assert beautify_string.beautify_string("The Beatles ", remove_leading_the=remove_leading_the) == "Beatles", "Test Leading The"
+    assert beautify_string.beautify_string(" The Beatles ", remove_leading_the=remove_leading_the) == "Beatles", "Test Leading The"
+    assert beautify_string.beautify_string("TheBeatles ", remove_leading_the=remove_leading_the) == "TheBeatles", "Test Leading The"
+    assert beautify_string.beautify_string("The-Beatles ", remove_leading_the=remove_leading_the) == "The-Beatles", "Test Leading The"
+    assert beautify_string.beautify_string(None, remove_leading_the=remove_leading_the) == None, "Test none"
+    assert beautify_string.beautify_string("The", remove_leading_the=remove_leading_the) == "The", "Test for a band named 'The'"
 
 
 
 def test_string_remove_whitespaces():
+    assert beautify_string._remove_not_needed_whitespaces("") == ""
     assert beautify_string._remove_not_needed_whitespaces(" lost in      space  Lost In Space ") == "lost in space Lost In Space", "Test spaces"
     assert beautify_string._remove_not_needed_whitespaces(" Test") == "Test"
     assert beautify_string._remove_not_needed_whitespaces("Test ") == "Test"
     assert beautify_string._remove_not_needed_whitespaces(" Test ") == "Test"
     assert beautify_string._remove_not_needed_whitespaces(" 123 456 ") == "123 456"
     assert beautify_string._remove_not_needed_whitespaces(" ") == ""
-    assert beautify_string._remove_not_needed_whitespaces("") == ""
     assert beautify_string._remove_not_needed_whitespaces("  ") == ""
 
 
 
 def test_string_unify_quotation_marks_and_accents():
+    assert beautify_string._unify_quotation_marks_and_accents("") == ""
     assert beautify_string._unify_quotation_marks_and_accents('"Great Song"') == "'Great Song'", "Quotation marks"
     assert beautify_string._unify_quotation_marks_and_accents("Tim´s") == "Tim's", "Accents"
     assert beautify_string._unify_quotation_marks_and_accents("Tim`s") == "Tim's", "Accents"
-    assert beautify_string._unify_quotation_marks_and_accents("") == ""
 
 
 def test_string_beautify_colons():
+    assert beautify_string._beautify_colons("") == ""
     assert beautify_string._beautify_colons("Deus Ex: Human Revolution") == "Deus Ex - Human Revolution", "Colon followed by whitespace"
     assert beautify_string._beautify_colons("He: Hello") == "He - Hello", "Colon followed by whitespace"
     assert beautify_string._beautify_colons("Deus:Ex") == "Deus-Ex", "Colon followed by non-whitespace 1"
@@ -67,10 +75,10 @@ def test_string_beautify_colons():
     assert beautify_string._beautify_colons(" : ") == " - ", "Colon surrounded by whitespace"
     assert beautify_string._beautify_colons(":Ex") == "-Ex", "Colon surrounded by whitespace"
     assert beautify_string._beautify_colons("Ex:") == "Ex-", "Colon surrounded by whitespace"
-    assert beautify_string._beautify_colons("") == ""
 
 
 def test_string_replace_special_characters():
+    assert beautify_string._replace_special_characters("") == ""
     assert beautify_string._replace_special_characters(";") == ","
     assert beautify_string._replace_special_characters("tl;dr") == "tl,dr"
     assert beautify_string._replace_special_characters("\\") == "-"
@@ -81,14 +89,14 @@ def test_string_replace_special_characters():
     assert beautify_string._replace_special_characters("WTF?") == "WTF"
     assert beautify_string._replace_special_characters("&") == "+"
     assert beautify_string._replace_special_characters("C&A") == "C+A"
-    assert beautify_string._replace_special_characters("") == ""
+    
 
 def test_string_fill_missing_space_after_comma():
     assert beautify_string._fill_missing_space_after_comma("") == ""
     assert beautify_string._fill_missing_space_after_comma("baum,stamm") == "baum, stamm"
     assert beautify_string._fill_missing_space_after_comma("10,5") == "10,5"    
     assert beautify_string._fill_missing_space_after_comma("5,neu") == "5, neu"
-    assert beautify_string._fill_missing_space_after_comma("neu,5") == "neu, 5" # TODO Better would be if this case would return 'neu, 5'
+    assert beautify_string._fill_missing_space_after_comma("neu,5") == "neu, 5"
 
 def test_string_remove_leading_the():
     assert beautify_string._remove_leading_the(remove_leading_the=True, text="") == ""
@@ -143,9 +151,12 @@ def test_string_capitalize_string():
 
 
 
-
-
 def test_track_number_beautification():
+    """
+    This tests the main/public method for beautifying track numbers. It contains all the (private) methods related to track numbers.
+    """
+    assert beautify_disc_and_track_number.beautify_track_number(track_number="01", helper_length_max=1, minimum_length=1) == "1", "Test removing existing leading zero 1"
+    assert beautify_disc_and_track_number.beautify_track_number(track_number="", helper_length_max=1, minimum_length=1) == ""
     assert beautify_disc_and_track_number.beautify_track_number(track_number="1", helper_length_max=1, minimum_length=1) == "1", "Test normal"
     assert beautify_disc_and_track_number.beautify_track_number(track_number="1", helper_length_max=2, minimum_length=2) == "01", "Test leading zeros 1"
     assert beautify_disc_and_track_number.beautify_track_number(track_number="1", helper_length_max=3, minimum_length=2) == "001", "Test leading zeros 2"
@@ -156,18 +167,36 @@ def test_track_number_beautification():
     assert beautify_disc_and_track_number.beautify_track_number(track_number="01", helper_length_max=2, minimum_length=2) == "01", "Test existing leading zero 1"
     assert beautify_disc_and_track_number.beautify_track_number(track_number="01", helper_length_max=3, minimum_length=2) == "001", "Test existing leading zero 2"
     assert beautify_disc_and_track_number.beautify_track_number(track_number="", helper_length_max=3, minimum_length=2) == "", "Test empty input"
-    assert beautify_disc_and_track_number.beautify_track_number(track_number="01", helper_length_max=1) == "1", "Test removing existing leading zero 1"
-    assert beautify_disc_and_track_number.beautify_track_number(track_number="001", helper_length_max=2) == "01", "Test removing existing leading zeros 2"
+    assert beautify_disc_and_track_number.beautify_track_number(track_number="01", helper_length_max=1, minimum_length=1) == "1", "Test removing existing leading zero 1"
+    assert beautify_disc_and_track_number.beautify_track_number(track_number="001", helper_length_max=2, minimum_length=2) == "01", "Test removing existing leading zeros 2"
     assert beautify_disc_and_track_number.beautify_track_number(track_number=" 1", helper_length_max=1, minimum_length=1) == "1", "Test spaces 1"
     assert beautify_disc_and_track_number.beautify_track_number(track_number=" 1 ", helper_length_max=1, minimum_length=1) == "1", "Test spaces 2"
     assert beautify_disc_and_track_number.beautify_track_number(track_number=None, helper_length_max=1, minimum_length=1) == None, "Test none"
 
 
+
+def test_mp3_extract_track_number_from_slash_format():
+    """
+    Testing the regex that extracts the track number from potential 'slash format'.
+    """
+    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("") == ""
+    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("01") == "01"
+    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("01/") == "01"
+    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("01/16") == "01"
+    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("1") == "1"
+    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("1/") == "1"
+    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("1/16") == "1"
+    assert beautify_disc_and_track_number.extract_track_number_from_slash_format(1) == "1"
+    assert beautify_disc_and_track_number.extract_track_number_from_slash_format(None) == None
+
+
+
+
 def test_mp3_tags_has_cd_string_in_folder_name():
     """
-    Testing the complex regex.
+    Testing the complex regex realted to strings that hint for multiple CDs in the folder name.
     """
-    
+    assert beautify_disc_and_track_number.has_cd_string_in_folder_name("") == False
     assert beautify_disc_and_track_number.has_cd_string_in_folder_name("cd 1") == True
     assert beautify_disc_and_track_number.has_cd_string_in_folder_name("cd3") == True
     assert beautify_disc_and_track_number.has_cd_string_in_folder_name("cd  2asd") == False
@@ -188,24 +217,9 @@ def test_mp3_tags_has_cd_string_in_folder_name():
     assert beautify_disc_and_track_number.has_cd_string_in_folder_name("_1 cd_") == True
     assert beautify_disc_and_track_number.has_cd_string_in_folder_name("2cdaasdf") == False
     assert beautify_disc_and_track_number.has_cd_string_in_folder_name("cd123455") == False
+    assert beautify_disc_and_track_number.has_cd_string_in_folder_name("ACDC 2010") == False
     assert beautify_disc_and_track_number.has_cd_string_in_folder_name(None) == False
     assert beautify_disc_and_track_number.has_cd_string_in_folder_name(50) == False
-
-
-def test_mp3_extract_track_number_from_slash_format():
-    """
-    Testing the regex that extracts the track number from potential 'slash format'.
-    """
-    
-    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("01") == "01"
-    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("01/") == "01"
-    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("01/16") == "01"
-    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("1") == "1"
-    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("1/") == "1"
-    assert beautify_disc_and_track_number.extract_track_number_from_slash_format("1/16") == "1"
-    assert beautify_disc_and_track_number.extract_track_number_from_slash_format(1) == "1"
-    assert beautify_disc_and_track_number.extract_track_number_from_slash_format(None) == None
-
 
 
 
@@ -213,9 +227,9 @@ def test_mp3_extract_track_number_from_slash_format():
 
 def test_mp3_tags_extract_year():
     """
-    Testing the regex that extract year (YYYY).
+    Testing the regex that extracts the year (YYYY).
     """
-    
+    assert beautify_date.extract_year("") == ""
     assert beautify_date.extract_year("1999-12-12") == "1999"
     assert beautify_date.extract_year("1999") == "1999"
     assert beautify_date.extract_year("20-12-1999") == "1999"
@@ -236,6 +250,8 @@ def test_mp3_tags_extract_year():
     assert beautify_date.extract_year(" 1999 ") == "1999"
     assert beautify_date.extract_year("1999 ") == "1999"
     assert beautify_date.extract_year("20/12/2020") == "2020"
+    assert beautify_date.extract_year("99") == "99"
+    assert beautify_date.extract_year("October '99") == "October '99" # TODO Adjust this case? But should be a total edge case possibly not worth the effort.
     assert beautify_date.extract_year(2020) == "2020"
     assert beautify_date.extract_year(1980) == "1980"
     assert beautify_date.extract_year(None) == None
