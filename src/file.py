1caf14be23e78694c4a412112c90b48ca884eac4
bjoerm <bjoern-menke@web.de>
2022-11-06 22:45:22 +0100

    Added sorting of track name suffixes.

--- src/file.py
+++ src/file.py
@@ -17,6 +17,7 @@ class File:
 
     def __init__(self, filepath: Path):
         self.filepath = filepath
+        self.filename = self.filepath.name
         self.tags_imported: Dict[str, str | bytes | POPM] = self.import_tags_as_dict()  # TODO Hier auf die Main-Method verweisen, welche den obigen groben Ablauf abbildet.
         self.tags_beautified = self.validate_and_beautify_tags()
 
@@ -45,7 +46,7 @@ class File:
         tags.TPE2 = StringBeautifier.beautify_string(tags.TPE2, remove_leading_the=True)
         tags.TPE1, tags.TIT2 = self.check_feat_in_artist(album_artist=tags.TPE1, track=tags.TIT2)
 
-        # TODO _sort_track_name_suffixes()
+        tags.TIT2 = self.sort_track_name_suffixes(tags.TIT2)
 
         return tags
 
@@ -83,16 +84,70 @@ class File:
 
         return album_artist, track
 
-    def export_tags_in_file(self):
-        # TODO A second public method that is used for writing output to files.
-        pass
+    @classmethod
+    def sort_track_name_suffixes(cls, track_name: Optional[str]) -> Optional[str]:
+        """Put any track names with multiple suffixes like (... Remix), (Acoustic), (Live ...), (Feat. ...) into an adequate order."""  # TODO If I only set Live at the end, then I don't need to over do it here.
+        if track_name is None:
+            return track_name
+
+        track_name, suffixes = cls._extract_suffixes(track_name)
+
+        if suffixes is None:
+            return track_name
+
+        suffixes = regex.findall(r"(\(.+?\))", suffixes, regex.IGNORECASE)  # Convert string into a list of strings.
+
+        selected_suffixes = {"live": None, "feat": None}
+
+        for suffix in suffixes.copy():  # Using a copy for iterating as items from the original list can be removed in this loop.
+            suffix_bare = cls._remove_string_noise(suffix)
+
+            for keyword in ["live", "feat"]:
+                # Looking for the keyword in the first and last word of the suffix.
+                if suffix_bare.split()[0] == keyword or suffix_bare.split()[-1] == keyword:
+                    selected_suffixes[keyword] = suffix
+                    suffixes.remove(suffix)
+
+        track_name_reordered = f'{track_name} {" ".join(suffixes)} {selected_suffixes.get("feat") or ""} {selected_suffixes.get("live") or ""}'
+
+        track_name_reordered = StringBeautifier._remove_not_needed_whitespaces(track_name_reordered)
+
+        return track_name_reordered
+
+    @staticmethod
+    def _extract_suffixes(track_name: str) -> Tuple[str, Optional[str]]:
+        """Checking the track name for multiple brackets."""
+        at_least_two_sets_of_brackets = regex.search(r"(.+?)(\(.+\)\s\(.+\))", track_name, regex.IGNORECASE)  # Search for strings with multiple brackets at the end.
+
+        if at_least_two_sets_of_brackets is None:  # Less than two sets of brackets found, no need for re-ordering.
+            return track_name, None
+
+        track_name_without_suffixes = at_least_two_sets_of_brackets.group(1).strip()  # Note that this would end with a space without the strip().
+        suffixes = at_least_two_sets_of_brackets.group(2)  # This is one single string.
+
+        return track_name_without_suffixes, suffixes
+
+    @staticmethod
+    def _remove_string_noise(text: str) -> str:
+        """
+        Gets rid of not needed characters like non-alphanumeric characters and capitalization from a string.
+        """
+
+        text = regex.sub(pattern=r"[^0-9a-zA-Z\s]+", repl="", string=text)  # Removing any non-alphanumeric characters.
+        text = text.lower()  # Uniquely transforming the word to lowercase.
+
+        return text
 
     def validate_tags_prior_update(self):
         # TODO Via Pydantic it should be checked whether the output is still according to the desired form. Should a new class be written for that?
         pass
 
-    def write_beautified_tag_to_files(self):
+    def write_beautified_tags_to_file(self):
         pass
+
+    def beautify_filename(self):
+        pass
+        # TODO Use another Filename class?
 
 
 if __name__ == "__main__":
