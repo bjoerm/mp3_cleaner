5d6d927a5699a2f1aa90466bf0e89098de991546
bjoerm <bjoern-menke@web.de>
2022-11-24 00:14:07 +0100

    Adjusted tests

--- tests/test_file.py
+++ tests/test_file.py
@@ -18,7 +18,7 @@ from mp3_file import MP3File
         ["None", None, None, None],
     ],
 )
-def test_check_alternative_fields(name, main_field, helper_field, expected_output):
+def test_check_fallback_tag_fields(name, main_field, helper_field, expected_output):
     assert MP3File.check_fallback_tag_fields(main_field=main_field, helper_field=helper_field) == (expected_output, None), name
 
 
@@ -34,7 +34,7 @@ def test_check_alternative_fields(name, main_field, helper_field, expected_outpu
         ["Feat No Brackets", "artist feat. abc", "track", "artist", "track (feat. abc)"],
     ],
 )
-def test_check_alternative_fields(name, arist_in, track_in, artist_out, track_out):
+def test_check_feat_in_artist(name, arist_in, track_in, artist_out, track_out):
     assert MP3File.check_feat_in_artist(album_artist=arist_in, track=track_in) == (artist_out, track_out), name
 
 
@@ -64,3 +64,22 @@ def test_extract_suffixes(name, input_track, output_track, output_suffix):
 )
 def test_sort_track_name_suffixes(name, input, expected_output):
     assert MP3File.sort_track_name_suffixes(track_name=input) == expected_output, name
+
+
+@pytest.mark.parametrize(
+    "name, number_current, leading_zeros, expected_output",
+    [
+        ["General", 1, 1, "1"],
+        ["General", 1, 2, "01"],
+        ["General", 1, 3, "001"],
+        ["General", 2, 1, "2"],
+        ["General", 2, 2, "02"],
+        ["General", 2, 3, "002"],
+        ["General", 10, 2, "10"],
+        ["General", 10, 3, "010"],
+        ["None", 1, None, None],
+        ["Edge Case that shouldn't be possible", 10, 1, "10"],
+    ],
+)
+def test_add_leading_zeros(name, number_current, leading_zeros, expected_output):
+    assert MP3File._add_leading_zeros(number_current=number_current, leading_zeros=leading_zeros) == expected_output, name
--- tests/test_folder.py
+++ tests/test_folder.py
@@ -6,13 +6,13 @@ from folder import Folder
 @pytest.mark.parametrize(
     "name, numbers, expected_output",
     [
-        ["Length 1", [1, 2, 3], 1],
-        ["Length 2", [1, 2, 10], 2],
-        ["Length 3", [1, 2, 100], 3],
-        ["Length 4", [1, 2, 1000], 4],
+        ["Length 1", ["1", "2", "3"], 1],
+        ["Length 2", ["1", "2", "10"], 2],
+        ["Length 3", ["1", "2", "100"], 3],
+        ["Length 4", ["1", "2", "1000"], 4],
         ["All None", [None, None, None], None],
-        ["One None", [1, 2, None], None],
-        ["Max 1", [1, 1, 1], None],
+        ["One None", ["1", "2", None], None],
+        ["Max 1", ["1", "1", "1"], None],
     ],
 )
 def test_calculate_leading_zeros(name, numbers, expected_output):
