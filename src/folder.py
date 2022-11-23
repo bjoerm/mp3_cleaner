15b3f92de094c4ffe3652ba9e12761c3df4dafa5
bjoerm <bjoern-menke@web.de>
2022-11-24 00:13:22 +0100

    More folder functionality

--- src/folder.py
+++ src/folder.py
@@ -20,6 +20,7 @@ class Folder:
     unwanted_files: List[str]
     mp3_filepaths: List[str] = field(init=False)
     mp3_files: List[MP3File] = field(init=False)
+    has_only_one_artist: Optional[bool] = field(init=False)
 
     def __post_init__(self):
         self.folder_full_output = self.generate_output_folder()
@@ -27,45 +28,78 @@ class Folder:
 
         self.copy_to_output_folder()
 
-        self.mp3_filepaths = glob.glob(
-            f"{str(self.folder_full_output)}/*.mp3", recursive=False
-        )  # Setting recursive to False is done as each folder with MP3 files in it should get (currently, 2022-11) its own call of the Folder class. Recursive=True would mess up some album wide defined methods in this class.
+        self.mp3_filepaths = self.fetch_mp3_filepaths()
 
-        self.mp3_files = self.init_file_classes()
+        self.mp3_files = self.init_mp3_file_classes()
 
-        self.calculate_folder_wide_tags()
+        self.unify_folder_wide_tags()
 
-    def calculate_folder_wide_tags(self):
-        self.calculate_leading_zeros(numbers=[file.tags.TRCK for file in self.mp3_files])
+    def copy_to_output_folder(self):
+        copytree(src=self.folder_full_input, dst=self.folder_full_output, ignore=ignore_patterns(*self.unwanted_files), dirs_exist_ok=True)  # Unwanted files won't be copied. One could also remove dirs_exist_ok to get an error as the output should ideally be empty before mp3 cleaning
 
-        [file.tags.TPOS for file in self.mp3_files]
+    def fetch_mp3_filepaths(self) -> List[str]:
+        return glob.glob(f"{str(self.folder_full_output)}/*.mp3", recursive=False)  # Recursive=True would mess up some album wide defined methods in this class.
 
-        pass
+    def init_mp3_file_classes(self) -> List[MP3File]:
+        return [MP3File(Path(i)) for i in self.mp3_filepaths]
+
+    def unify_folder_wide_tags(self):
+        """Beautify the tags that rely on information from album/folder level."""
+
+        self.unify_track_and_album_numbers()
+        self.has_only_one_artist = self.check_artist_uniqueness(artists=[file.tags.TPE1 for file in self.mp3_files])
+
+        # TODO Continue here with checks for Nones. [file.tags.TDRC for file in self.mp3_files]
+
+    def unify_track_and_album_numbers(self):
+        leading_zeros_track = self.calculate_leading_zeros(numbers=[file.tags.TRCK for file in self.mp3_files])
+
+        # Ensure that the tracks always have a length of 2 characters.
+        if leading_zeros_track is not None:
+            leading_zeros_track = max(2, leading_zeros_track)
+
+        leading_zeros_album = self.calculate_leading_zeros(numbers=[file.tags.TPOS for file in self.mp3_files])
+
+        for file in self.mp3_files:
+            file.beautify_track_and_album_number(leading_zeros_track=leading_zeros_track, leading_zeros_album=leading_zeros_album)
 
     @staticmethod
-    def calculate_leading_zeros(numbers: List[Optional[int]]) -> Optional[int]:
+    def check_artist_uniqueness(artists: List[Optional[str]]) -> Optional[bool]:
+        # TODO Maybe rewrite this so that not a boolean is returned, but the artist name or None is returned instead. Maybe even a generalised "extract_and_check_uniqueness" method can be used for artist, year, album title?
+        # TODO Write unittest!
+        if None in artists:
+            raise ValueError("At least one file is without artist tag.")
+
+        artists_unique = set(artists)
+
+        if len(artists_unique) == 1:
+            return True
+        elif len(artists_unique) > 1:
+            return False
+        else:
+            raise ValueError("This shouldn't happen.")
+
+    @staticmethod
+    def calculate_leading_zeros(numbers: List[Optional[str]]) -> Optional[int]:
         """Calculates the highest disc or track number that was encountered. If any number is missing (=None) or the highest number is 1, then None will be returned."""
 
         if None in numbers:
             return None
 
         else:
-            highest_track_number = max(numbers)
+            numbers_as_int = [int(i) for i in numbers]
+            highest_number = max(numbers_as_int)
 
-            if highest_track_number == 1:
+            if highest_number == 1:
                 return None
 
-            highest_track_number = str(highest_track_number)
-            length_string = len(highest_track_number)
+            highest_number = str(highest_number)
+            length_string = len(highest_number)
             return length_string
 
-    # TODO After the init album wide tags should be beautified (e.g. leading zeros in track numbers). This should be done via a (public) classmethod in File that gets e.g. the number of leading zeros from here (the album class). That way the File classes (inside thie file_classes list) are all updated.
-    # TODO The same approach should be done for the other steps on the file (tag) level like writing tages etc.
-    # Thus the following pseudo code shows the next steps:
-    # self.calculate_leading_zeros_track()
-    # self.calculate_leading_zeros_album()
-    # self.any_other_album_wide_field_or_information() e.g. whether all have the same artist.
-    # self.check_missing_years()  # Log a warning if any of the Files lacks a year, track number, artist, title, if all/some (?) of the other songs have that field.
+    # One the album level:
+    ## self.check_missing_years()  # Log a warning if any of the Files lacks a year, track number, artist, title, if all/some (?) of the other songs have that field.
+    # Validate the tags one more (with an output Pydantic class)
     # self.convert_pydantic_tags_to_mutagen_object (maybe this is not even needed and all can be directly handled in the write method below)
     # self.write_beautified_tags()
     # self.beautify_filenames()
@@ -84,15 +118,6 @@ class Folder:
 
         return folder_full_output
 
-    def copy_to_output_folder(self):
-        copytree(src=self.folder_full_input, dst=self.folder_full_output, ignore=ignore_patterns(*self.unwanted_files), dirs_exist_ok=True)  # Unwanted files won't be copied. One could also remove dirs_exist_ok to get an error as the output should ideally be empty before mp3 cleaning
-
-    def init_file_classes(self) -> List[MP3File]:
-
-        list_of_file_classes = [MP3File(Path(i)) for i in self.mp3_filepaths]
-
-        return list_of_file_classes
-
 
 if __name__ == "__main__":
 
@@ -100,4 +125,5 @@ if __name__ == "__main__":
         config = tomllib.load(f)
 
     abc = Folder(folder_full_input=Path("input/aMP3/"), folder_main_input=Path("input/"), folder_main_output=Path("output/"), unwanted_files=config["unwanted_files"])
+    abc.mp3_files[0].tags
     abc.folder_full_output
