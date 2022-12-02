1556be6db8b6170aa13a1602c646029c3c6fde05
bjoerm <bjoern-menke@web.de>
2022-12-02 23:59:07 +0100

    Parallel tag beautification.

--- src/mp3_cleaner.py
+++ src/mp3_cleaner.py
@@ -1,14 +1,14 @@
-# TODO Add a pydantic class for the folder that has a list (with at least one entry of tagsexported class)
+# TODO Add a pydantic class for the folder that has a list (with at least one entry of tagsexported class). That would also detect errors.
+# TODO Think about a logging.
 
-
-import glob
 import logging
 import shutil
+from datetime import datetime
 from pathlib import Path
 from typing import List
 
 import tomllib
-from tqdm import trange
+from tqdm.contrib.concurrent import thread_map
 
 from folder.main import Folder
 from pydantic_models.config_model import Config
@@ -24,10 +24,11 @@ def main():
 
     mp3_folders = _find_mp3_folders(input_path=config.input_path)  # TODO .absolute()?
 
-    for i in trange(len(mp3_folders)):
-        logging.info(f"Processing: {mp3_folders[i]}")
+    def worker(folder_i: int):
+        """Works on a folder at a time. Thus, can be used in parallel."""
+        Folder(folder_full_input=mp3_folders[folder_i], folder_main_input=config.input_path, folder_main_output=config.output_path, unwanted_files=config.unwanted_files)
 
-        Folder(folder_full_input=mp3_folders[i], folder_main_input=config.input_path, folder_main_output=config.output_path, unwanted_files=config.unwanted_files)
+    thread_map(worker, range(len(mp3_folders)), desc="Folders", unit=" folders")  # Uses all cores. Number of parallel workers can be limited via max_workers.
 
 
 def _find_mp3_folders(input_path: Path) -> List[Path]:
@@ -45,7 +46,12 @@ def _find_mp3_folders(input_path: Path) -> List[Path]:
 
 
 if __name__ == "__main__":
+    start_time = datetime.now()
+    print(f'Script started at {start_time.strftime("%H:%M:%S")}.')
 
     main()
+
+    finish_time = datetime.now()
+    print(f'Script finished at {finish_time.strftime("%H:%M:%S")}.\nTook {((finish_time - start_time).total_seconds())} seconds.')
 
     logging.info("End of script reached.")
